f8e9be583dd892977744a2fcb4a28a896cd966fe
ckyle121 <carolinekyle21@gmail.com>
2022-02-04 11:33:23 -0600

    dynamically write the html page

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name,github), err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Checkout index.html')
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -132,6 +124,12 @@ const promtProject = portfolioData => {
 
 promptUser()
     .then(promtProject)
-    .then(projectAnswers => {
-        console.log(portfolioData);
+    .then(portfolioData => {
+        const pageHTML = generatepage();
+
+        fs.writeFile('./index/html', pageHTML, err => {
+            if (err) throw new Error (err);
+
+            console.log('Page created! Check out index.html')
+        });
     });
